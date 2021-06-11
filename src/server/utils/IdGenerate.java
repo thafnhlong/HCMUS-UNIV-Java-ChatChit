c69f9dbd060b910226f51257ce0562e08bc93723
thafnhlong <thafnhlong@gmail.com>
2021-06-11 21:23:04 +0700

    fix warning syn immutable

--- src/server/utils/IdGenerate.java
+++ src/server/utils/IdGenerate.java
@@ -1,14 +1,16 @@
 package server.utils;
 
 public class IdGenerate {
+    private final static Object lock;
     private static Long uniqueId;
 
     static {
         uniqueId=0L;
+        lock=new Object();
     }
 
     public static long generate(){
-        synchronized(uniqueId){
+        synchronized(lock){
             uniqueId+=1L;
         }
         return uniqueId;
